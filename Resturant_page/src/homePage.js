680bd9e0ab73e1fcd1b6982278931473983c8d13
Anil-Gollapalli <anilgollapalli2@gmail.com>
2024-11-22 13:47:10 +0530

    Safe commit message

--- Resturant_page/src/homePage.js
+++
@@ -1,9 +0,0 @@
-export default function loadHomePage() {
-    const contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = `
-      <h1>Welcome to Our Restaurant!</h1>
-      <img src="restaurant.jpg" alt="Restaurant">
-      <p>Enjoy our delicious meals and great service.</p>
-    `;
-  }
-  
