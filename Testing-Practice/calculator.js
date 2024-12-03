d509cc433932de6654e424565fad15489988a9d6
Anil-Gollapalli <anilgollapalli2@gmail.com>
2024-12-03 18:00:12 +0530

    Safe commit message

--- Testing-Practice/calculator.js
+++
@@ -1,8 +0,0 @@
-const calculator = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => (b !== 0 ? a / b : 'Error: Division by zero')
-};
-
-module.exports = calculator;
--- Testing-Practice/reverseString.js
+++
@@ -1,6 +0,0 @@
-function reverseString(str) {
-    if (typeof str !== 'string') return '';
-    return str.split('').reverse().join('');
-}
-
-module.exports = reverseString;
